d975b680b7a87a579238ebe195675d073efecb6f
Saarthak Verma <saarthakverma739@gmail.com>
2022-03-26 16:10:43 +0530

    added timer and error messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'dart:async';
 import 'dart:math';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -18,6 +18,8 @@ class Level1 extends StatefulWidget {
 }
 
 class _Level1State extends State<Level1> {
+  int flag=1;
+  double opacityState=1;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -25,27 +27,12 @@ class _Level1State extends State<Level1> {
           data:const MediaQueryData(),
           child: Scaffold(
               appBar: AppBar(
-                title: const Text("LEVEL 1"),
+                title: const Text("COLOR PICK"),
                 backgroundColor: correctColor,
               ),
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:
-                [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [const Text("Pick "),Container(
-                      color: correctColor,
-                      width: MediaQuery.of(context).size.width/4,
-                      height: MediaQuery.of(context).size.width/4,
-                    )
-                    ],
-                  ),
-                  level1(MediaQuery.of(context).size.width,MediaQuery.of(context).size.height),
-                ],
-              ),
-              backgroundColor: Colors.grey
+              body:
+                    level1(MediaQuery.of(context).size.width,MediaQuery.of(context).size.height),
+                    backgroundColor: Colors.grey
           )
       ),
     );
@@ -62,192 +49,319 @@ class _Level1State extends State<Level1> {
       }
     }
   }
-  List<Color> randColor = [Colors.red,Colors.yellow,Colors.blue,Colors.green,Colors.orange,Colors.brown,Colors.deepPurple,Colors.lightGreen,Colors.teal];
-  Color correctColor=Colors.red;
+  late Timer _timer=Timer(Duration(seconds: 10), (){});
+  void timer(){
+    futureFlag=0;
+    if(_timer.isActive) {
+      _timer.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: 1500), () {
+      // print(flag);
+      if(futureFlag==0){
+        flag=0;
+        error=Text("Time exceeded");
+        doStuff();
+      }
+    });
+  }
   void doStuff(){
     int x=generateRandom()+1;
     // print(x);
     // print(randColor[0]);
-    setState(() {
-      rotateBy(x);
-      correctColor=randColor[generateRandom()];
-    });
+    if(flag!=0) {
+      setState(() {
+        rotateBy(x);
+        correctColor = randColor[generateRandom()];
+      });
+    }
+    else{
+      setState(() {
+        opacityState=0;
+      });
+    }
 
   }
+  List<Color> randColor = [Colors.red,Colors.yellow,Colors.blue,Colors.green,Colors.orange,Colors.brown,Colors.deepPurple,Colors.lightGreen,Colors.teal];
+  Color correctColor=Colors.red;
 
-  Container level1(double deviceWidth,double deviceHeight) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(border: Border.all()),
-      child: Column(
+  int futureFlag=1;
+  Text error=Text("Wrong Choice");
+
+  Column level1(double deviceWidth,double deviceHeight) {
+
+    if(flag==1) {
+      futureFlag=0;
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
-            children: [
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[0],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
-                    ),
-                  ),
-                  onTap: (){
-                    if(randColor[0]==correctColor) {
-                      doStuff();
-                    }
-                  },
-                ),
-              ),
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[1],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
-                    ),
-                  ),
-                  onTap: (){
-                    if(randColor[1]==correctColor) {
-                      doStuff();
-                    }
-                  },
-                ),
-              ),
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[2],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
-                    ),
-                  ),
-                  onTap: (){
-                    if(randColor[2]==correctColor) {
-                      doStuff();
-                    }
-                  },
-                ),
-              )
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [const Text("Pick "),Container(
+              color: correctColor,
+              width: MediaQuery.of(context).size.width/4,
+              height: MediaQuery.of(context).size.width/4,
+            )
             ],
           ),
-          Row(
-            children: [
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[3],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
+          Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(border: Border.all()),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[0],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[0] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            int i=0;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  onTap: (){
-                    if(randColor[3]==correctColor) {
-                      doStuff();
-                    }
-                  },
-                ),
-              ),
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[4],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[1],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[1] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            error=Text("You picked wrong");
+                            setState(() {});
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  onTap: (){
-                    if(randColor[4]==correctColor) {
-                      doStuff();
-                    }
-                  },
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+
+                            color: randColor[2],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[2] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[5],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
+                Row(
+                  children: [
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[3],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[3] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  onTap: (){
-                    if(randColor[5]==correctColor) {
-                      doStuff();
-                    }
-                  },
-                ),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[6],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[4],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[4] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  onTap: (){
-                    if(randColor[6]==correctColor) {
-                      doStuff();
-                    }
-                  },
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[5],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[5] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[7],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
+                Row(
+                  children: [
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[6],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[6] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  onTap: (){
-                    if(randColor[7]==correctColor) {
-                      doStuff();
-                    }
-                  },
-                ),
-              ),
-              Expanded(
-                flex: generateRandom()+1,
-                child: InkWell(
-                  child: IgnorePointer(
-                    child: Container(
-                      color:randColor[8],
-                      width: deviceWidth/10,
-                      height: deviceWidth/4,
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[7],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[7] == correctColor) {
+                            flag=1;
+                            futureFlag=1;
+                            doStuff();
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  onTap: (){
-                    if(randColor[8]==correctColor) {
-                      doStuff();
-                    }
-                  },
+                    Expanded(
+                      flex: generateRandom() + 1,
+                      child: InkWell(
+                        child: IgnorePointer(
+                          child: Container(
+                            color: randColor[8],
+                            width: deviceWidth / 10,
+                            height: deviceWidth / 4,
+                          ),
+                        ),
+                        onTap: () {
+                          if (randColor[8] == correctColor) {
+                            flag=1;
+                            doStuff();
+                            futureFlag=1;
+                            // _timer.cancel();
+                            timer();
+                          }
+                          else{
+                            flag=0;
+                            setState(() {});
+                          }
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ],
-      ),
-    );
+      );
+    }
+    else{
+      futureFlag=1;
+        return Column(
+          children: [
+            Container(child: error),
+          ],
+        );
+    }
   }
 }
